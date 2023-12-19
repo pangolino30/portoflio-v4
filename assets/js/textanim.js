eda72b267f5e166d2c575c6aca99d3887b12a892
Lucas Spitzer <lucasspitzer30@gmail.com>
2023-12-19 08:31:08 +0100

    Changement de l'animation de texte et ajout projets

--- assets/js/textanim.js
+++ assets/js/textanim.js
@@ -3,4 +3,5 @@ var typewriter = new Typewriter(typingtext, {
 	loop: true,
 	delay: 75,
 });
-typewriter.typeString("Je suis étudiant en BTS SIO").pauseFor(800).deleteChars(19).typeString("passionné par l'inforatique").pauseFor(800).deleteChars(13).typeString("le développement web").pauseFor(800).deleteChars(20).typeString("la modélisation 3D").pauseFor(800).deleteChars(18).typeString("le design.").pauseFor(800).start();
+typewriter.typeString("Je suis étudiant en BTS SIO.").pauseFor(800).deleteChars(20).typeString("passionné par l'informatique.").pauseFor(800).deleteChars(15).typeString("le développement web.").pauseFor(800).deleteChars(21).typeString("la modélisation 3D.").pauseFor(800).deleteChars(19).typeString("le design.").pauseFor(800).deleteChars(37).typeString("Encore là ?").pauseFor(2000).start();
+// .deleteChars(37).typeString("J'aime les chats 😻").pauseFor(800)
